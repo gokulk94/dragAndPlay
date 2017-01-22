4ab4fb1a535e30b6310ba5e6419d35815902a156
Gokul K <gokulk@qburst.com>
2017-01-22 16:11:07 +0530

    add basic form feature(NOT COMPLETE)

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/**
+ * NOTE: This is kindof a demo. Did'nt really consider about any use case for this. 
+ * Just wanted to learn mouse events and this is a good start... :)
+*/
+
 function dragon(selector) {
   var el = selector;
   var dragStartX, dragStartY, currentElPosX, currentElPosY;
@@ -19,13 +24,23 @@ function dragon(selector) {
     el.style.transform = `translate3d(${x}px, ${y}px, 0px)`;
   }
 
-  function removeEvents() {
+  function removeEvents(e) {
+    var container = document.querySelector('.page');
+    var contB = container.getBoundingClientRect();
+    if(e.clientX > contB.left && e.clientY > contB.top && el.tagName !== 'INPUT') {
+      var div = document.createElement('input');
+      div.type = 'text';
+      div.style.transform = `translate3d(${e.clientX - contB.left}px, ${e.clientY - contB.top}px, 0px)`;
+      container.appendChild(div);
+      dragon(div);
+      el.style.transform = `translate3d(0px, 0px, 0px)`;
+    }
     document.removeEventListener('mousemove',handler);
     document.removeEventListener('mouseup',removeEvents);
   }
 }
 
-var draggableElements = document.querySelectorAll('ul li');
+var draggableElements = document.querySelectorAll('.sideSelector ul li');
 
 draggableElements.forEach(function(item){
   new dragon(item);
