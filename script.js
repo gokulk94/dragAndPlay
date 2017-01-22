113c01e86921bc180999a84d5583281ed7f9ba7b
Gokul K <gokulk@qburst.com>
2017-01-22 23:48:03 +0530

    add function for getting coordinates of elements

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ function dragon(selector) {
   var el = selector;
   var dragStartX, dragStartY, currentElPosX, currentElPosY;
   var originOffSetX = el.offsetLeft,
-      originOffSetY = el.offsetTop;
-  el.addEventListener('mousedown',function(e) {
+    originOffSetY = el.offsetTop;
+  el.addEventListener('mousedown', function (e) {
     dragStartX = e.clientX;
     dragStartY = e.clientY;
     var pos = el.getBoundingClientRect();
     currentElPosX = pos.left;
     currentElPosY = pos.top;
-    document.addEventListener('mousemove',handler);
-    document.addEventListener('mouseup',removeEvents);
+    document.addEventListener('mousemove', handler);
+    document.addEventListener('mouseup', removeEvents);
   });
 
   function handler(e) {
@@ -27,22 +27,52 @@ function dragon(selector) {
   function removeEvents(e) {
     var container = document.querySelector('.page');
     var contB = container.getBoundingClientRect();
-    if(e.clientX > contB.left && e.clientY > contB.top && el.tagName !== 'INPUT') {
-      var div = document.createElement('input');
-      div.type = 'text';
-      div.style.transform = `translate3d(${e.clientX - contB.left}px, ${e.clientY - contB.top}px, 0px)`;
-      container.appendChild(div);
-      dragon(div);
-      el.style.transform = `translate3d(0px, 0px, 0px)`;
+
+    /*
+      All the logic for the formBuilder feature should be here.
+      Yet to decide on how to structure this
+    */
+
+    if (e.clientX > contB.left && e.clientY > contB.top && el.tagName !== 'INPUT' && el.tagName !== 'SPAN' && e.clientX < (contB.width + contB.left)) {
+      insertItem(el.getAttribute('data-purpose'), e.clientX, e.clientY, contB, el, container);
     }
-    document.removeEventListener('mousemove',handler);
-    document.removeEventListener('mouseup',removeEvents);
+    document.removeEventListener('mousemove', handler);
+    document.removeEventListener('mouseup', removeEvents);
   }
+}
+
+function insertItem(purpose, clientX, clientY, clientRect, el, container) {
+  var div;
+  if(purpose === 'inputField') {
+    div = document.createElement('input');
+    div.type = 'text';
+    div.classList.add('inputField');
+  } else if(purpose === 'signBox') {
+    div = document.createElement('span');
+    div.classList.add('signBox');
+  }
+  div.style.transform = `translate3d(${clientX - clientRect.left}px, ${clientY - clientRect.top}px, 0px)`;
+  container.appendChild(div);
+  dragon(div);
+  el.style.transform = `translate3d(0px, 0px, 0px)`;
 }
 
 var draggableElements = document.querySelectorAll('.sideSelector ul li');
 
-draggableElements.forEach(function(item){
+draggableElements.forEach(function (item) {
   new dragon(item);
 });
 
+function sendData() {
+  var content = document.querySelectorAll('.page *');
+  var coordinates = [];
+  content.forEach(function(item) {
+    var t = item.style.transform.split('(')[1].split(',');
+    coordinates.push({
+      purpose: item.className,
+      coordinateX: t[0],
+      coordinateY: t[1]
+    });
+  });
+  console.log(coordinates);
+}
